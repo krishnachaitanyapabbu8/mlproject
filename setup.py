a0e30890997741af49b973ba4043425c0acb8d8e
Krishnachaitanya Pabbu <krishnachaitanyapabbu8@gmail.com>
2023-06-01 13:55:12 +0530

    setup and requirement

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.read().splitlines()
+    requirements = [req.strip() for req in requirements if req.strip() != HYPHEN_E_DOT]
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='krishnachaitanya',
+    author_email='krishnachaitanyapabbu8@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
